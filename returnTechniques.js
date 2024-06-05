8624b8ace79800de7d01e07907e3315a3eb01bac
Everton Redivo <61854502+evertonredivo@users.noreply.github.com>
2024-06-05 16:44:36 -0300

    Create returnTechniques.js

---
+++ returnTechniques.js
@@ -0,0 +1,9 @@
+## return Techniques
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
