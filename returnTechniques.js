34965be485e6694997f3b8da66d4fd9732a52b3f
Everton Redivo <61854502+evertonredivo@users.noreply.github.com>
2024-06-05 18:07:36 -0300

    Update and rename returnTechniques.js to techniques.js

--- returnTechniques.js
+++
@@ -1,9 +0,0 @@
-## return Techniques
-
-function hasPlayerWonTheRound(player, computer) {
-  return (
-    (player === "Rock" && computer === "Scissors") ||
-    (player === "Scissors" && computer === "Paper") ||
-    (player === "Paper" && computer === "Rock")
-  );
-}
---
+++ techniques.js
@@ -0,0 +1,22 @@
+// return Technique
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+
+// Conditional (ternary) operator inside Template literals (Template strings)
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
+
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${ playerScore === 3 ? "Player" : "Computer" } has won the game!`; // see placeholder here
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
+
+};
